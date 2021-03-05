73c8a104ca2d05f938e7ba0f52c2eea9dd90c797
Setup <Setup@EPINHYDW07CA.princeton.epam.com>
2021-03-05 15:26:03 +0530

    Hotel Reservation Service

---
+++ src/test/java/com/epam/hotel/reservation/ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.hotel.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
